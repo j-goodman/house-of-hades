992ebf7421f06d97fedd316a8a5187fdc8bfdf73
John Goodman <john.goodman@fluentcity.com>
2018-04-10 19:54:16 -0400

    Abstract crawler goTo action.

--- js/crawler.js
+++ js/crawler.js
@@ -13,9 +13,19 @@ Crawler.prototype.checkSurroundings = function () {
 Crawler.prototype.goTo = function (door) {
     let startRoom = this.room
     let endRoom = this.room === door.to ? door.from : door.to
-    startRoom.monsters = startRoom.monsters.filter(mon => {
-        return mon !== this
-    })
-    endRoom.monsters.push(this)
+    let arrayName = ''
+    if (this.room.monsters.includes(this)) {
+      	arrayName = 'monsters'
+    } else if (
+      	arrayName = 'items'
+    )
+
+    if (arrayName) {
+        startRoom[arrayName] = startRoom[arrayName].filter(ob => {
+            return ob !== this
+        })
+        endRoom[arrayName].push(this)
+    }
+
     this.room = endRoom
 }
